1cc69dbc2b7ad49c7a10d9ac9edb5d983402578e
Core Rose <cr67.rose@gmail.com>
2024-11-05 11:40:19 +0100

    Create Service and Interface for Rating

---
+++ P7CreateRestApi/Services/Interface/IRatingService.cs
@@ -0,0 +1,13 @@
+﻿using Dot.Net.WebApi.Controllers.Domain;
+
+namespace P7CreateRestApi.Services.Interface
+{
+    public interface IRatingService
+    {
+        Task<IEnumerable<Rating>> GetAllRatingsAsync();
+        Task<Rating> GetRatingByIdAsync(int id);
+        Task<Rating> CreateRatingAsync(Rating rating);
+        Task<Rating> UpdateRatingAsync(int id,Rating rating);
+        Task<bool> DeleteRatingAsync(int id);
+    }
+}
