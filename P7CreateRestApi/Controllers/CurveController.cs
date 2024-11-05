5b4b4fc27b8aa9e14e3c32176084a8d57b95650d
Core Rose <cr67.rose@gmail.com>
2024-11-05 10:14:16 +0100

    Add Logger in CurveController

--- P7CreateRestApi/Controllers/CurveController.cs
+++ P7CreateRestApi/Controllers/CurveController.cs
@@ -2,6 +2,7 @@ using Dot.Net.WebApi.Domain;
 using Microsoft.AspNetCore.Mvc;
 using P7CreateRestApi.Models.DTOs.CurveDTOs;
 using P7CreateRestApi.Services.Interface;
+using System.Security.Cryptography;
 
 namespace Dot.Net.WebApi.Controllers
 {
@@ -10,16 +11,30 @@ namespace Dot.Net.WebApi.Controllers
     public class CurveController : ControllerBase
     {
         private readonly ICurvePointService _curvePointService;
+        private readonly ILogger<CurveController> _logger;
 
-        public CurveController(ICurvePointService curvePointService)
+        public CurveController(ICurvePointService curvePointService, ILogger<CurveController> logger)
         {
             _curvePointService = curvePointService;
+            _logger = logger;
         }
 
+        // GET: api/curve/list
         [HttpGet("list")]
         public async Task<IActionResult> GetAllCurvePoints()
         {
+            _logger.LogInformation("Received request to GET all curves at {Time}", DateTime.UtcNow);
             var curvePoints = await _curvePointService.GetAllCurvePointsAsync();
+
+            if(!curvePoints.Any())
+            {
+                _logger.LogWarning("No curves found at {Time}", DateTime.UtcNow);
+            }
+            else
+            {
+                _logger.LogInformation("Fetched {Count} curves at {Time}", curvePoints.Count(), DateTime.UtcNow);
+            }
+
             var curvePointDtos = curvePoints.Select(cp => new CurvePointDTO
             {
                 Id = cp.Id,
@@ -33,10 +48,17 @@ namespace Dot.Net.WebApi.Controllers
             return Ok(curvePointDtos);
         }
 
+        //POST: api/curve/add
         [HttpPost("add")]
         public async Task<IActionResult> AddCurvePoint([FromBody] CurvePointCreateDTO curvePointCreateDto)
         {
-            if (!ModelState.IsValid) return BadRequest(ModelState);
+            _logger.LogInformation("Received request  to CREATE a new curve at {Time}", DateTime.UtcNow);
+
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning("Invalid model state for AddCurvePoint request at {Time}", DateTime.UtcNow);
+                return BadRequest(ModelState);
+            }
 
             var curvePoint = new CurvePoint
             {
@@ -47,14 +69,25 @@ namespace Dot.Net.WebApi.Controllers
             };
 
             var createdCurvePoint = await _curvePointService.CreateCurvePointAsync(curvePoint);
+
+            _logger.LogInformation("Created curve with ID {CurveId} at {Time}", createdCurvePoint.Id, DateTime.UtcNow);
+
             return CreatedAtAction(nameof(GetAllCurvePoints), new { id = createdCurvePoint.Id }, createdCurvePoint);
         }
 
+        //PUT: api/curve/update/{id}
         [HttpPut("update/{id}")]
         public async Task<IActionResult> UpdateCurvePoint(int id, [FromBody] CurvePointUpdateDTO curvePointUpdateDto)
         {
+            _logger.LogInformation("Received request to UPDATE bid with ID {CurveId} at {Time}", id, DateTime.UtcNow);
+
             var existingCurvePoint = await _curvePointService.GetCurvePointByIdAsync(id);
-            if (existingCurvePoint == null) return NotFound();
+
+            if (existingCurvePoint == null)
+            {
+                _logger.LogWarning("Curve with ID {CurveId} not found for update at {Time}", id, DateTime.UtcNow);
+                return NotFound();
+            }
 
             existingCurvePoint.CurveId = curvePointUpdateDto.CurveId;
             existingCurvePoint.AsOfDate = curvePointUpdateDto.AsOfDate;
@@ -62,16 +95,29 @@ namespace Dot.Net.WebApi.Controllers
             existingCurvePoint.CurvePointValue = curvePointUpdateDto.CurvePointValue;
 
             await _curvePointService.UpdateCurvePointAsync(id, existingCurvePoint);
+
+            _logger.LogInformation("Updated bid with ID {CurveId} at {Time}", id, DateTime.UtcNow);
+
             return Ok(existingCurvePoint);
         }
 
+        //DELETE: api/curve/{id}
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCurvePoint(int id)
         {
+            _logger.LogInformation("Received request to DELETE bid with ID {BidId} at {Time}", id, DateTime.UtcNow);
+
             var curvePoint = await _curvePointService.GetCurvePointByIdAsync(id);
-            if (curvePoint == null) return NotFound();
+            if (curvePoint == null)
+            {
+                _logger.LogWarning("Bid with ID {BidId} not found for deletion at {Time}", id, DateTime.UtcNow);
+                return NotFound();
+            }
 
             await _curvePointService.DeleteCurvePointAsync(id);
+            _logger.LogInformation("Deleted curve with ID {CurveId} at {Time}", id, DateTime.UtcNow);
+
+
             return NoContent();
         }
     }
